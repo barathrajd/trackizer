15282d026fdafc0336d59acc7498b1b35d3be213
barathdeva757@gmail.com <barathdeva757@gmail.com>
2024-09-07 22:34:52 +0530

    chore: base setup for the app

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,11 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function HomeScreen() {
+export default function AuthLayout() {
 	return (
 		<SafeAreaView>
 			<View>
-				<Text>HomeScreen</Text>
+				<Text style={{ color: "white" }}>Textadf</Text>
 			</View>
 		</SafeAreaView>
 	);
